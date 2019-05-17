6077619077eef109698823755d8f497d7a763b98
Tariq <tariq.abusheikh@gmail.com>
2019-05-17 14:07:13 -0500

    Code cleanup of `MerchantBusinessTest`:  * Use `self` for static method calls

--- tests/MerchantBusinessTest.php
+++ tests/MerchantBusinessTest.php
@@ -2,31 +2,32 @@
 
 namespace Omnipay\Braintree;
 
-use DateTime;
-use DateTimeZone;
 use Omnipay\Tests\TestCase;
 
 class MerchantBusinessTest extends TestCase
 {
+    /** @var MerchantBusiness */
+    protected $business;
+
     public function setUp()
     {
         $this->business = new MerchantBusiness();
     }
-    
+
     public function testConstructWithParams()
     {
         $business = new MerchantBusiness(array('legalName' => 'Hoochie\s Dollar Store'));
-        $this->assertSame('Hoochie\s Dollar Store', $business->getLegalName());
+        self::assertSame('Hoochie\s Dollar Store', $business->getLegalName());
     }
 
     public function testInitializeWithParams()
     {
         $business = new MerchantBusiness;
         $business->initialize(array('legalName' => 'Hoochie\s Dollar Store'));
-        $this->assertSame('Hoochie\s Dollar Store', $business->getLegalName());
+        self::assertSame('Hoochie\s Dollar Store', $business->getLegalName());
     }
 
-    public function testGetParamters()
+    public function testGetParameters()
     {
         $card = new MerchantBusiness(array(
             'legalName' => 'Hoochie\s Dollar Store',
@@ -37,64 +38,64 @@ class MerchantBusinessTest extends TestCase
         ));
 
         $parameters = $card->getParameters();
-        $this->assertSame('Hoochie\s Dollar Store', $parameters['legalName']);
-        $this->assertSame('620 W South St', $parameters['address1']);
-        $this->assertSame('Millsburg', $parameters['city']);
-        $this->assertSame('AR', $parameters['state']);
-        $this->assertSame('72015', $parameters['postCode']);
+        self::assertSame('Hoochie\s Dollar Store', $parameters['legalName']);
+        self::assertSame('620 W South St', $parameters['address1']);
+        self::assertSame('Millsburg', $parameters['city']);
+        self::assertSame('AR', $parameters['state']);
+        self::assertSame('72015', $parameters['postCode']);
     }
 
     public function testDbaName()
     {
         $this->business->setDbaName('Hoochie\s Dollar Store');
-        $this->assertEquals('Hoochie\s Dollar Store', $this->business->getDbaName());
+        self::assertEquals('Hoochie\s Dollar Store', $this->business->getDbaName());
     }
 
     public function testLegalName()
     {
         $this->business->setLegalName('Hoochie\s Dollar Store');
-        $this->assertEquals('Hoochie\s Dollar Store', $this->business->getLegalName());
+        self::assertEquals('Hoochie\s Dollar Store', $this->business->getLegalName());
     }
 
     public function testPhone()
     {
         $this->business->setPhone('501.778.3151');
-        $this->assertEquals('501.778.3151', $this->business->getPhone());
+        self::assertEquals('501.778.3151', $this->business->getPhone());
     }
 
     public function testTaxId()
     {
         $this->business->setTaxId('98-7654321');
-        $this->assertEquals('98-7654321', $this->business->getTaxId());
+        self::assertEquals('98-7654321', $this->business->getTaxId());
     }
 
     public function testAddress1()
     {
         $this->business->setAddress1('620 W South St');
-        $this->assertEquals('620 W South St', $this->business->getAddress1());
+        self::assertEquals('620 W South St', $this->business->getAddress1());
     }
 
     public function testCity()
     {
         $this->business->setCity('Millsburg');
-        $this->assertEquals('Millsburg', $this->business->getCity());
+        self::assertEquals('Millsburg', $this->business->getCity());
     }
 
     public function testPostcode()
     {
         $this->business->setPostcode('72015');
-        $this->assertEquals('72015', $this->business->getPostcode());
+        self::assertEquals('72015', $this->business->getPostcode());
     }
 
     public function testState()
     {
         $this->business->setState('AR');
-        $this->assertEquals('AR', $this->business->getState());
+        self::assertEquals('AR', $this->business->getState());
     }
 
     public function testEmail()
     {
         $this->business->setEmail('vaughn@HoochiesDollarStore.com');
-        $this->assertEquals('vaughn@HoochiesDollarStore.com', $this->business->getEmail());
+        self::assertEquals('vaughn@HoochiesDollarStore.com', $this->business->getEmail());
     }
 }
